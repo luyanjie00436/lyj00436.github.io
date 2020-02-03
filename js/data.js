9eddb5b849ff7db995df319167964234317d06fc
luyanjie00436 <18359027950@163.com>
2020-02-03 10:52:08 +0800

    修改（将包含姓名的源代码改成其他的）

--- js/data.js
+++ js/data.js
@@ -3,169 +3,169 @@ var data = {
         {
             "id": 1,
             "imgUrl": "img/1.png",
-            "name ": "林杰",
+            "name ": "姓名1",
             isWin: false
         },
         {
             "id": 2,
             "imgUrl": "img/2.png",
-            "name ": "周明",
+            "name ": "姓名2",
             isWin: false
         },
         {
             "id": 3,
             "imgUrl": "img/3.png",
-            "name ": "陈倩媛",
+            "name ": "姓名3",
             isWin: false
         },
         {
             "id": 4,
             "imgUrl": "img/4.png",
-            "name ": "翁振元",
+            "name ": "姓名4",
             isWin: false
         },
         {
             "id": 5,
             "imgUrl": "img/5.png",
-            "name ": "许键锋",
+            "name ": "姓名5",
             isWin: false
         },
         {
             "id": 6,
             "imgUrl": "img/6.png",
-            "name ": "郑强",
+            "name ": "姓名6",
             isWin: false
         },
         {
             "id": 7,
             "imgUrl": "img/7.png",
-            "name ": "林永清",
+            "name ": "姓名7",
             isWin: false
         },
         {
             "id": 8,
             "imgUrl": "img/8.png",
-            "name ": "刘爱清",
+            "name ": "姓名8",
             isWin: false
         },
         {
             "id": 9,
             "imgUrl": "img/9.png",
-            "name ": "吴承焱",
+            "name ": "姓名9",
             isWin: false
         },
         {
             "id": 10,
             "imgUrl": "img/10.png",
-            "name ": "陈坤元",
+            "name ": "姓名10",
             isWin: false
         },
         {
             "id": 11,
             "imgUrl": "img/11.png",
-            "name ": "李进国",
+            "name ": "姓名11",
             isWin: false
         },
         {
             "id": 12,
             "imgUrl": "img/12.png",
-            "name ": "刘昌棋",
+            "name ": "姓名12",
             isWin: false
         },
         {
             "id": 13,
             "imgUrl": "img/13.png",
-            "name ": "刘建园",
+            "name ": "姓名13",
             isWin: false
         },
         {
             "id": 14,
             "imgUrl": "img/14.png",
-            "name ": "朱国静",
+            "name ": "姓名14",
             isWin: false
         },
         {
             "id": 15,
             "imgUrl": "img/15.png",
-            "name ": "曹其伟",
+            "name ": "姓名15",
             isWin: false
         },
         {
             "id": 16,
             "imgUrl": "img/16.png",
-            "name ": "王鸿卿",
+            "name ": "姓名16",
             isWin: false
         },
         {
             "id": 17,
             "imgUrl": "img/17.png",
-            "name ": "成江",
+            "name ": "姓名17",
             isWin: false
         },
         {
             "id": 18,
             "imgUrl": "img/18.png",
-            "name ": "毛桂莲",
+            "name ": "姓名18",
             isWin: false
         },
         {
             "id": 19,
             "imgUrl": "img/19.png",
-            "name ": "吴榕凤",
+            "name ": "姓名19",
             isWin: false
         },
         {
             "id": 20,
             "imgUrl": "img/20.png",
-            "name ": "卞娟",
+            "name ": "姓名20",
             isWin: false
         },
         {
             "id": 21,
             "imgUrl": "img/21.png",
-            "name ": "林娟",
+            "name ": "姓名21",
             isWin: false
         },
         {
             "id": 22,
             "imgUrl": "img/22.png",
-            "name ": "卓宜爽",
+            "name ": "姓名22",
             isWin: false
         },
         {
             "id": 23,
             "imgUrl": "img/23.png",
-            "name ": "陈怡",
+            "name ": "姓名23",
             isWin: false
         },
         {
             "id": 24,
             "imgUrl": "img/24.png",
-            "name ": "王芳",
+            "name ": "姓名24",
             isWin: false
         },
         {
             "id": 25,
             "imgUrl": "img/25.png",
-            "name ": "陈丽娇",
+            "name ": "姓名25",
             isWin: false
         },
         {
             "id": 26,
             "imgUrl": "img/26.png",
-            "name ": "陈蕊",
+            "name ": "姓名26",
             isWin: false
         },
         {
             "id": 27,
             "imgUrl": "img/27.png",
-            "name ": "刘浩",
+            "name ": "姓名27",
             isWin: false
         },       
         {
             "id": 29,
             "imgUrl": "img/29.png",
-            "name ": "陆彦捷",
+            "name ": "姓名28",
             isWin: false
         }
     ]
